81efd47106de5bb18a60e2ced6ae976861e2e8b7
Mohsen Ranjbar <mimrahe@gmail.com>
2017-06-05 22:44:27 +0430

    methods documented

--- sqliteorm/src/main/java/ir/mimrahe/sqliteorm/DatabaseHelper.java
+++ sqliteorm/src/main/java/ir/mimrahe/sqliteorm/DatabaseHelper.java
@@ -35,6 +35,12 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseHelperIn
         modifyDatabase(db, oldVersion + 1, newVersion);
     }
 
+    /**
+     * reads database version files and modifies database
+     * @param db database instance
+     * @param oldVersion older version of database
+     * @param newVersion newer version of database
+     */
     private void modifyDatabase(SQLiteDatabase db, int oldVersion, int newVersion){
         for (int i = oldVersion; i <= newVersion; i++) {
             for (String modification : compileScheme(i)) {
@@ -61,6 +67,11 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseHelperIn
         super.close();
     }
 
+    /**
+     * reads database version file
+     * @param databaseVersion version of database
+     * @return array of database version file lines
+     */
     private ArrayList<String> compileScheme(int databaseVersion) {
         ArrayList<String> modifications = new ArrayList<>();
 
